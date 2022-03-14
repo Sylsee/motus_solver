dc956312bfc4f8785e2f4b22b975aa6e5b08d942
Sylsee <sylvio.poliart@gmail.com>
2022-03-14 18:55:10 +0100

    motus solver

--- motus.c
+++ motus.c
@@ -6,7 +6,7 @@
 /*   By: spoliart <spoliart@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 15:17:40 by spoliart          #+#    #+#             */
-/*   Updated: 2022/03/14 18:28:48 by spoliart         ###   ########.fr       */
+/*   Updated: 2022/03/14 18:50:14 by spoliart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,32 +21,32 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-bool motus(const char *s, const char *charset, const char *s_contains, const char *red, const char *yellow)
+bool motus(const char *word, const char *exclude, const char *contains, const char *red, const char *yellow)
 {
-	size_t	contains = 0;
-	char	*tmp = strdup(s_contains);
+	size_t	nb_contains = 0;
+	char	*tmp = strdup(contains);
 
-	for (size_t i = 0; s[i]; i++) {
-		for (size_t j = 0; charset[j]; j++) {
-			if (s[i] == charset[j]) {
+	for (size_t i = 0; word[i]; i++) {
+		for (size_t j = 0; exclude[j]; j++) {
+			if (word[i] == exclude[j]) {
 				free(tmp);
 				return (false);
 			}
 		}
 		for (size_t j = 0; tmp[j]; j++) {
-			if (s[i] == tmp[j]) {
+			if (word[i] == tmp[j]) {
 				tmp[j] = '=';
-				contains++;
+				nb_contains++;
 				break ;
 			}
 		}
-		if (red[i] != '.' && red[i] != s[i])
+		if (red[i] != '.' && red[i] != word[i])
 			return (false);
-		else if (yellow[i] == s[i])
+		else if (yellow[i] == word[i])
 			return (false);
 	}
 	free(tmp);
-	if (strlen(s_contains) != contains)
+	if (strlen(contains) != nb_contains)
 		return (false);
 	return (true);
 }
@@ -58,7 +58,7 @@ int main(int ac, char **av)
 		return (EXIT_FAILURE);
 	}
 	size_t	printed = 1;
-	char	*charset = av[1];
+	char	*exclude = av[1];
 	char	*contains = av[2];
 	char	*red = av[3];
 	char	*yellow = av[4];
@@ -82,7 +82,7 @@ int main(int ac, char **av)
 
 	size_t	i;
 	for (i = 0; words[i]; i++) {
-		if (motus(words[i], charset, contains, red, yellow) == true)
+		if (motus(words[i], exclude, contains, red, yellow) == true)
 			printf("word %ld: [%s]\n", printed++, words[i]);
 	}
 	printf("total given words: %ld\n", i);
